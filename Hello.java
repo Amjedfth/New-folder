3e572cf880b7188c44dcb1527eb062f4161c7deb
Amgad Fateh <amjdfeth@gmail.com>
2024-02-27 21:13:18 +0800

    make it short

--- Hello.java
+++ Hello.java
@@ -1,12 +1,9 @@
-/**
- * Hello
- */
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("my name is ");
-        System.out.println("amgad");
-        System.out.println("I am 24 years old");
+
+       
+
+        System.out.println("hello I am amgad");
     }
 }
