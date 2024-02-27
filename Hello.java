8139824c1c9f9baafc2774962bb7125b49eb7916
Amgad Fateh <amjdfeth@gmail.com>
2024-02-27 21:11:49 +0800

    added some information

--- Hello.java
+++ Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("hello world");
         System.out.println("my name is ");
         System.out.println("amgad");
+        System.out.println("I am 24 years old");
     }
 }
